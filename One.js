335e7caa4e4772ab64f9d48045e010807c8506a0
Snowfoxxxx <spanishmax1996@gmail.com>
2021-07-12 23:40:13 +0000

    F...g code is run now

---
+++ One.js
@@ -0,0 +1,7 @@
+//var num = parseInt(prompt('Введите число в диапазоне от 25 до 99'));
+var num = 26;
+var result;
+if (num >= 25 && num <= 99) {
+    console.log(num);
+    //alert(num ? 'Well done' : 'Число не в диапазоне. Попробуйте еще раз');
+}
