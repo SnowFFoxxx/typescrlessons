e854117d380231c9c5aef8bbfd86d9dce866e63e
SnowFFoxxx <spanishmaxxx@gmail.com>
2021-07-31 12:14:21 +0000

    Fixed bugs

--- cypress/integration/Autotests/RegistrationForm.ts
+++ cypress/integration/Autotests/RegistrationForm.ts
@@ -1,16 +1,17 @@
 describe ('Sign in', () => {
-    it('Try to sign in', () => {
-        cy.visit('https://ita-social-projects.github.io/GreenCityClient/#/') //visit site
-        cy.url().should('include', 'https://ita-social-projects.github.io/GreenCityClient');
-    });
+    it('Is url valid', () => {
+    cy.visit('https://ita-social-projects.github.io/GreenCityClient/#/');
+    cy.url().should('include', 'https://ita-social-projects.github.io/GreenCityClient');
+    cy.get(`[class="header_sign-up-btn secondary-global-button"]`).click();
+});
 });
 
 describe ('Is email input work', () => {
     it('type doe234@gmail.com into email and check it visible', () => {
-        cy.contains('Sign up').click(); //click on sign up button
+        cy.get(`[class="header_sign-up-btn secondary-global-button"]`)
         cy.get('[id="email"]')
             .type('doe234@gmail.com');
-        cy.should('be.visible', 'doe234@gmail.com');
+        cy.should('have.value','doe234@gmail.com');
     });
 });
 
@@ -18,7 +19,7 @@ describe ('Is username input work', () => {
     it('type JohnDoe into username and check it visible', () => {
         cy.get('[id="firstName"]')
             .type('JohnDoe');
-        cy.should('be.visible', 'JohnDoe');
+        cy.should('have.value', 'JohnDoe');
     });
 });
 
@@ -26,10 +27,10 @@ describe ('Is password input work', () => {
     it('type Q1w2e3#34 into password and check it visible', () => {
         cy.get('[id="password"]')
             .type('Q1w2e3#34');
-        cy.should('be.visible', 'Q1w2e3#34');
+        cy.should('have.value', 'Q1w2e3#34');
         cy.get('[id="repeatPassword"]')
             .type('Q1w2e3#34');
-        cy.should('be.visible', 'Q1w2e3#34');
+        cy.should('have.value', 'Q1w2e3#34');
     });
 });
 
